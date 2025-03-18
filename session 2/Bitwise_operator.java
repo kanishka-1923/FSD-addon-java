e70b9d1139173544b56072c63fffbe2d34d2e8b3
kanishka-1923 <kkanishkaforever@gmail.com>
2025-03-18 15:25:30 +0530

    Add files via upload

---
+++ session 2/Bitwise_operator.java
@@ -0,0 +1,21 @@
+package programs;
+
+public class Bitwise_operator {
+	
+		
+
+		public static void main(String[] args) {
+
+			int a = 5,b = 3;
+
+			System.out.println("Bitwise AND: "+ (a & b));
+
+			System.out.println("Bitwise OR: "+ (a | b));
+
+			System.out.println("Bitwise XOR: "+ (a ^ b));
+
+			System.out.println("Bitwise NOT: "+ (~a));
+
+					
+}
+}
---
+++ session 2/inc_dec.java
@@ -0,0 +1,11 @@
+package programs;
+
+public class inc_dec {
+	public static void main(String[] args) {
+		int a = 456 , b = 5;
+		
+		System.out.println("Post Increment"+(a++ + --a + a++ ));
+				
+				
+	}
+}
